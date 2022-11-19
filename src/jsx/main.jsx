c5b442ba93ee9e035f7df55d5cb1efd6f83bc8fe
Izzminhal Akmal <izzmin97@gmail.com>
2022-11-19 22:28:08 +0800

    Implemented Quest generation based on data

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -1,11 +1,33 @@
 const React = require("react");
 const ReactDOM = require("react-dom/client");
 
-function questList() {
-    return <div className="root-container"></div>
+let testData = [
+    {
+        questTitle: "Yo mama",
+        questDesc: "Doing your mom LOL"
+    },
+
+    {
+        questTitle: "Wassup, doc?",
+        questDesc: "Emergency meeting due to sus"
+    }
+];
+
+function Quest(props) {
+    return <div>
+        <p id="quest-title">{props.title}</p>
+        <p id="quest-desc">{props.description}</p>
+    </div>
+}
+
+function QuestList(props) {
+    return <div className="root-container">
+        {props.arr.map((data, index) => 
+            <Quest key={index} title={data.questTitle} description={data.questDesc}/>
+            )}
+    </div>
 }
 
 // Set root for quest list
-const questListSection = document.getElementById("quest-list");
-const root = ReactDOM.createRoot(questListSection);
-root.render(< questList />);
+const root = ReactDOM.createRoot(document.getElementById("quest-list"));
+root.render(<QuestList arr={testData} />);
