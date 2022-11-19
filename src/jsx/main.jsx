f2547342d9e024e1b879bab62ade0f5416f35dbe
Izzminhal Akmal <izzmin97@gmail.com>
2022-11-19 21:49:56 +0800

    Made root containter

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -2,7 +2,7 @@ const React = require("react");
 const ReactDOM = require("react-dom/client");
 
 function questList() {
-
+    return <div className="root-container"></div>
 }
 
 // Set root for quest list
