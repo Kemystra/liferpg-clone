f936d6ad6456d6dc2087caa79f3f670c265e73d8
Izzminhal Akmal <izzmin97@gmail.com>
2022-11-19 21:47:42 +0800

    Made root containter for quest list

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -1,3 +1,11 @@
 const React = require("react");
 const ReactDOM = require("react-dom/client");
 
+function questList() {
+
+}
+
+// Set root for quest list
+const questListSection = document.getElementById("quest-list");
+const root = ReactDOM.createRoot(questListSection);
+root.render(< questList />);
