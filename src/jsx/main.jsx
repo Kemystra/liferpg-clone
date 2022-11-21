611f1f977185ddee588388fa0aedef00df3fade0
Izzminhal Akmal <izzmin97@gmail.com>
2022-11-21 11:14:07 +0800

    Working implementation of reading Supabase

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -1,40 +1,64 @@
+/**
+ * Terminology:
+ * 'set': set a data value
+ * 'get': get a data value
+ * 'fetch': primarily in APIs
+ */
+
 import * as React from 'react';
 import * as ReactDOM from  'react-dom/client';
 import { createClient } from '@supabase/supabase-js'
 
+// =============================== Fetching Data Section ===============================
 // Supabase stuff
 const supabaseUrl = 'https://qxjxlinixufdsasuwmvy.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4anhsaW5peHVmZHNhc3V3bXZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg4NjkxMjgsImV4cCI6MTk4NDQ0NTEyOH0.EeYpdNC4Mh9Vd1olibBqPz2RbXk_mz8aTNYwamxcm_I'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'
+                    + '.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4anhsaW5peHVmZHNhc3V3bXZ5Iiwi'
+                    + 'cm9sZSI6ImFub24iLCJpYXQiOjE2Njg4NjkxMjgsImV4cCI6MTk4NDQ0NTEyOH0'
+                    + '.EeYpdNC4Mh9Vd1olibBqPz2RbXk_mz8aTNYwamxcm_I';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-let testData = [
-    {
-        questTitle: "Yo mama",
-        questDesc: "Doing your mom LOL"
-    },
-
-    {
-        questTitle: "Wassup, doc?",
-        questDesc: "Emergency meeting due to sus"
-    }
-];
-
+// ============================== React Components Section ==============================
 // ! React components MUST start with uppercase
-function Quest(props) {
+const Quest = props => {
     return <div>
-        <p id="quest-title">{props.title}</p>
-        <p id="quest-desc">{props.description}</p>
+        <p class="quest-title">{props.title}</p>
+        <p class="quest-desc">{props.description}</p>
     </div>
 }
 
-function QuestList(props) {
+const QuestList = props => {
+    const [questsData, setQuestsData] = React.useState([]);
+
+    React.useEffect(() => {
+        // If directly use async, cleanup function of useEffect would be a Promise
+        // so, kinda bad?
+        (async () => {
+            let resp = await supabase
+            .from('Quests')
+            .select('title, description');
+
+            if(resp.error) {
+                console.error(`Error encountered in fetching data: ${JSON.stringify(resp.error)}`);
+            }
+            
+            setQuestsData(resp.data);
+        })();
+    }, []);
+
     return <div className="root-container">
-        {props.arr.map((data, index) => 
-            <Quest key={index} title={data.questTitle} description={data.questDesc}/>
+        {questsData.map((data, index) => 
+            <Quest key={index} title={data.title} description={data.description}/>
             )}
     </div>
 }
 
+// ==================================== Main Section ====================================
+// If you have code that needs to do asynchronous stuff, put it here please :)
+(async () => {
+
+})();
+
 // Set root for quest list
 const root = ReactDOM.createRoot(document.getElementById("quest-list"));
-root.render(<QuestList arr={testData} />);
+root.render(<QuestList arr={[]} />);
