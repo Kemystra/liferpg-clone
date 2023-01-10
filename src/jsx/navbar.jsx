bf9abf112995051cac27100719c96a6608aaa92f
Izzminhal Akmal <izzmin97@gmail.com>
2023-01-10 14:51:13 +0800

    Moved navbar into JSX

---
+++ src/jsx/navbar.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Navbar = () => {
+    return <ul id="navbar">
+        <a href="" id="quests">quests</a>
+        <a href="" id="mastery">mastery</a>
+        <a href="" id="category">category</a>
+    </ul>
+}
+
+module.exports = { Navbar };
